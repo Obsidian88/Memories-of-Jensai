7354458e5597f6f9924cc8187b8b6e46ff15753b
wanko <phil.wanko@googlemail.com>
2016-08-20 18:30:24 +0200

    Added Animation test scene

---
+++ Assets/CharacterAnimation.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimation : MonoBehaviour {
+
+    public int Facing = 6; // facing direction, clock: 3 is right, 6 is down, 9 is left, 12 is up etc.
+
+    private Animator Anim;
+    private SpriteRenderer Renderer;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = 0f;
+
+        if (moveVertical == 0)
+        {
+            Anim.SetBool("idle", true);
+        }
+        else if (moveVertical > 0)
+        {
+            Anim.SetBool("idle", false);
+            Facing = 12;
+            Anim.SetInteger("facing", 12);
+            Anim.SetFloat("vSpeed", moveVertical);
+        }
+        else if (moveVertical < 0)
+        {
+            Anim.SetBool("idle", false);
+            Facing = 6;
+            Anim.SetInteger("facing", 6);
+            Anim.SetFloat("vSpeed", moveVertical);
+        }
+
+
+    }
+
+
+}
---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour
+{
+    
+    private float MaxSpeed = 15f;
+
+    private Rigidbody Rigidbody;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = 0f;
+
+        Rigidbody.velocity = new Vector3(moveHorizontal * MaxSpeed, Rigidbody.velocity.y, moveVertical * MaxSpeed);
+        
+    }
+
+
+}
