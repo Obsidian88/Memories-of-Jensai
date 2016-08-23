b546924359a08483f61fc06c51a9e85179aa686a
wanko <phil.wanko@googlemail.com>
2016-08-23 14:57:21 +0200

    gave the torches shadows

---
+++ Assets/ReceiveShadows.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReceiveShadows : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().receiveShadows = true;
+        GetComponent<SpriteRenderer>().shadowCastingMode= UnityEngine.Rendering.ShadowCastingMode.On;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/SortingLayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortingLayer : MonoBehaviour {
+
+    public string Layer;
+    public int Order;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().sortingLayerName = Layer;
+        GetComponent<Renderer>().sortingOrder = Order;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
