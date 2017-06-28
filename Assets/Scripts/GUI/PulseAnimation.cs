1b18ca02cce42135c2bfe55237fa8a1384143db4
Obsidian88 <megamuhkuh@gmx.de>
2017-06-28 12:04:03 +0200

    Fixed pulse-animation on GUI

---
+++ Assets/Scripts/GUI/PulseAnimation.cs
@@ -0,0 +1,22 @@
+﻿// A simple script used for fading the opacity of text.
+// It can be used to make it look less static
+// and be subtly animated
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PulseAnimation : MonoBehaviour {
+
+    [Tooltip("The higher the value the more subtle is the opacitychange")]
+    public float OpacityMultiplier = 0.8f;
+
+    [Tooltip("The higher the value the slower the transition")]
+    public float TimeMultiplier = 4f;
+	
+    // Updates once per frame
+    void Update()
+    {
+		Image Image = gameObject.GetComponent<Image>();
+        Image.color = new Color(Image.color.r, Image.color.g, Image.color.b, Mathf.PingPong(Time.time / TimeMultiplier, 1 - OpacityMultiplier) + OpacityMultiplier);
+    }
+}
