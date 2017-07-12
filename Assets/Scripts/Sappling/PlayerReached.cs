d4f321b89545d2c06da0ac96ff0ea7ebaed4d868
Roland <Megamuhkuh@gmx.de>
2017-07-12 12:03:35 +0200

    Sappling now follows player, scroll has now explorationdetail, outer stairpart ported to prototypescene

---
+++ Assets/Scripts/Sappling/PlayerReached.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerReached : MonoBehaviour {
+
+    public GameObject AI;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Character")
+        {
+            AI.SetActive(false);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Character")
+        {
+            AI.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Sappling/SapplingAI.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SapplingAI : MonoBehaviour
+{
+    public float RunSpeed = 8f;
+    public GameObject parentRigidbody;
+
+    private Transform target = null;
+    private Rigidbody Rigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody = parentRigidbody.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = parentRigidbody.transform.position;
+        if (target != null)
+        {
+            Vector3 direction = target.position - transform.position;
+            direction.Normalize();
+            Rigidbody.velocity = direction * RunSpeed;
+        }
+        else
+        {
+            Rigidbody.velocity = new Vector3(0, 0, 0);
+        }
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.tag == "Character")
+        {
+            target = obj.transform;
+        }
+    }
+
+    void OnTriggerExit(Collider obj)
+    {
+        if (obj.tag == "Character" && target != null)
+        {
+            target = null;
+        }
+    }
+}
